8ed5b8f41489ad7b51a092c71197a8692f6e15c4
Stanisław Małolepszy <sta@malolepszy.org>
2025-08-15 23:29:01 +0200

    Randomize sprite frame timing

--- src/scenes/blu_fighter.ts
+++ src/scenes/blu_fighter.ts
@@ -1,23 +1,20 @@
 import {ai_fighter} from "../components/com_ai_fighter.js";
 import {animate_sprite} from "../components/com_animate_sprite.js";
 import {collide2d} from "../components/com_collide2d.js";
-import {disable} from "../components/com_disable.js";
 import {health} from "../components/com_health.js";
 import {local_transform2d} from "../components/com_local_transform2d.js";
 import {move2d} from "../components/com_move2d.js";
 import {render2d} from "../components/com_render2d.js";
 import {spatial_node2d} from "../components/com_spatial_node2d.js";
 import {Game, Layer} from "../game.js";
-import {Has} from "../world.js";
 
 export function blueprint_fighter(game: Game, is_player: boolean) {
     return [
         spatial_node2d(),
         local_transform2d(undefined, 0, [1.5, 1.5]),
         collide2d(true, Layer.Object, Layer.Terrain | Layer.Object, [1.5, 1.5]),
-        render2d(is_player ? "6" : "7"), // Different sprites for player vs opponent
-        animate_sprite({6: 0.4, 7: 0.4}),
-        disable(Has.AnimateSprite),
+        render2d("13"),
+        animate_sprite({13: Math.random(), 14: Math.random()}),
         health(3), // 3 HP for each fighter
         move2d(2, 0), // Movement controlled by AI (2 units/sec, no rotation)
         ai_fighter(-1), // AI will find target automatically
