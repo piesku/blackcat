7e35671732830d1226b6fdea60f66ada68aea231
Stanisław Małolepszy <sta@malolepszy.org>
2025-09-12 07:54:50 +0200

    Add collision and continuous damage handling to shadow particle

--- src/blueprints/particles/blu_shadow_particle.ts
+++ src/blueprints/particles/blu_shadow_particle.ts
@@ -1,9 +1,12 @@
 import {Tile} from "../../../sprites/spritesheet.js";
+import {collide2d} from "../../components/com_collide2d.js";
+import {deal_damage} from "../../components/com_deal_damage.js";
 import {label} from "../../components/com_label.js";
 import {lifespan} from "../../components/com_lifespan.js";
 import {local_transform2d} from "../../components/com_local_transform2d.js";
 import {particle, ParticleType} from "../../components/com_particle.js";
 import {order, render2d} from "../../components/com_render2d.js";
+import {Layer} from "../../game.js";
 
 export function blueprint_shadow_particle() {
     return [
@@ -17,7 +20,12 @@ export function blueprint_shadow_particle() {
         // Shadow particle physics and behavior
         particle(ParticleType.Shadow, 0.0, [0.05, 0.05], 2), // spread, finalScale, fadeOut
 
-        // TODO add deal_damage
+        // Collision and damage - continuous damage dealing
+        collide2d(true, Layer.Particle, Layer.Player, 1),
+        deal_damage(1, {
+            cooldown: 0.1, // Short cooldown for continuous damage
+            destroy_on_hit: false, // Don't destroy on hit - continuous damage
+        }),
 
         lifespan(3),
     ];
